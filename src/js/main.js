731c3d308e38821df05c7a14b5ee2e2818e85a99
v-sproj <sprojcarova.val@seznam.cz>
2024-12-21 19:53:38 +0100

    prostě commit

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,82 @@ import '../scss/styles.scss'
 // Import all of Bootstrap's JS
 import {Dropdown} from 'bootstrap';
 
+
+import { Carousel, Fancybox } from '@fancyapps/ui';
+
+import '@fancyapps/ui/dist/carousel/carousel.css';
+import '@fancyapps/ui/dist/fancybox/fancybox.css';
+
+import { Thumbs } from '@fancyapps/ui/dist/carousel/carousel.thumbs.esm';
+import '@fancyapps/ui/dist/carousel/carousel.thumbs.css';
+
+const productCarousel = new Carousel(
+    document.getElementById('productCarousel'),
+    {
+      transition: 'slide',
+      preload: 3, // Smoother navigation when using lazy loaded images
+  
+      Dots: false,
+      Thumbs: {
+        type: 'classic',
+        Carousel: {
+          dragFree: false,
+          slidesPerPage: 'auto',
+          Navigation: true,
+  
+          axis: 'x',
+          breakpoints: {
+            '(min-width: 768px)': {
+              axis: 'y',
+            },
+          },
+        },
+      },
+    },
+    { Thumbs }
+  );
+  
+  Fancybox.bind('[data-fancybox="gallery"]', {
+    compact: false,
+    idle: false,
+    dragToClose: false,
+    contentClick: () =>
+      window.matchMedia('(max-width: 578px), (max-height: 578px)').matches
+        ? 'toggleMax'
+        : 'toggleCover',
+  
+    animated: false,
+    showClass: false,
+    hideClass: false,
+  
+    Hash: false,
+    Thumbs: false,
+  
+    Toolbar: {
+      display: {
+        left: [],
+        middle: [],
+        right: ['close'],
+      },
+    },
+  
+    Carousel: {
+      transition: 'fadeFast',
+      preload: 3,
+    },
+  
+    Images: {
+      zoom: false,
+      Panzoom: {
+        panMode: 'mousemove',
+        mouseMoveFactor: 1.1,
+      },
+    },
+  });
+
+
+
+
+  
+  
+
--- vite.config.js
+++ vite.config.js
@@ -49,3 +49,4 @@ export default {
         port: 8080
     }
 }
+
