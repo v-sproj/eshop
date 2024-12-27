5d040a6e6fc293ffb0e2ce5917e5b3494bc0ec7c
v-sproj <sprojcarova.val@seznam.cz>
2024-12-27 11:44:49 +0100

    commit asi

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@
 import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
-import {Dropdown} from 'bootstrap';
+import { Dropdown } from 'bootstrap';
 
 
 import { Fancybox } from "@fancyapps/ui";
@@ -15,11 +15,24 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 AOS.init({
-    duration: 700,
+  duration: 700,
+});
+
+document.querySelectorAll('a[data-bs-toggle="tab"]').forEach(tab => {
+  tab.addEventListener('shown.bs.tab', function (event) {
+    const activatedTab = document.querySelector(event.target.getAttribute('href'));
+    const allTabs = document.querySelectorAll('.tab-pane');
+
+    allTabs.forEach(tabContent => tabContent.classList.add('d-none'));
+    activatedTab.classList.remove('d-none');
   });
-  
+});
 
 
-  
-  
+
+
+
+
+
+
 
