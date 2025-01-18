c9dab8f5dd093720cc4c064fcf73bc9ffe15c635
v-sproj <sprojcarova.val@seznam.cz>
2025-01-18 11:51:34 +0100

    další kousek

--- src/js/main.js
+++ src/js/main.js
@@ -39,8 +39,28 @@ document.querySelectorAll('.nav-link').forEach(link => {
   });
 });
 
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
 
-$(element).inputSpinner(config);
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
+
+
+
 
 
 
